0156e9e7b179390cd2ce93aeca12664e4ba508fb
OTAKE-Soya <oyassun@gmail.com>
2020-12-09 11:45:56 +0000

    usersのshowにbookの投稿フォーム作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,22 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to user_path(current_user.id) #リダイレクト先は最終的には投稿したbookのshow#
   end
   def index
     @user = current_user
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
