c973efa1c5bb9fa301cb08e45746159012ece9e7
OTAKE-Soya <oyassun@gmail.com>
2020-12-09 11:23:39 +0000

    サイドバーのプロフィール部分テンプレート作成、各ページで呼び出し

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,17 @@
 class BooksController < ApplicationController
   def index
+    @user = current_user
   end
-  
+
   def new
   end
-  
+
   def create
   end
-  
+
   def edit
   end
-  
+
   def destroy
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+  def index
+    @user = current_user
+  end
 end
