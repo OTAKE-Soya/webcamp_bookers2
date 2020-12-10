702d55e991a26c7d4b3b0cb2c6371423e0bc72ca
OTAKE-Soya <oyassun@gmail.com>
2020-12-10 03:30:21 +0000

    edit画面作成、edit機能実装（画像はまだ）

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,17 +5,25 @@ class UsersController < ApplicationController
     @books = @user.book
   end
 
-  def create
-
-
-  end
   def index
     @user = current_user
     @book = Book.new
     @users = User.all
   end
 
-  private
+  def edit
+    @user = User.find(params[:id])
+  end
 
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :introduction)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
-  resources :users, only: [:index, :edit, :show]
+  resources :users, only: [:index, :edit, :show, :update]
   resources :books, only: [:index, :edit, :show, :destroy, :create]
   # post 'users/:id' => 'users#create'
 end
