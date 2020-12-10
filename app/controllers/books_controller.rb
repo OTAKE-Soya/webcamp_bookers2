83de4c1d353a866d53ff187db86f0e4854ecfd3f
OTAKE-Soya <oyassun@gmail.com>
2020-12-10 16:44:03 +0000

    ログイン・非ログイン時のヘッダー修正、bookのshow作成、アプリ内のリンクをだいたい実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,7 +2,7 @@ class BooksController < ApplicationController
   def index
     @user = current_user
     @book = Book.new
-    
+
     @books = Book.all
   end
 
@@ -27,6 +27,7 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @new_book = Book.new
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :edit, :show, :update]
   resources :books, only: [:index, :edit, :show, :destroy, :create]
   # post 'users/:id' => 'users#create'
+  get "home/about" => "homes#about"
 end
