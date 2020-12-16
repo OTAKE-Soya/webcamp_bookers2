fba2cf61ba05d6a502f72f471524dbde113c0dd8
OTAKE-Soya <oyassun@gmail.com>
2020-12-16 06:52:02 +0000

    コメント投稿にバリデートつけてエラー表示実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,16 @@
 class CommentsController < ApplicationController
   def create
     book = Book.find(params[:book_id])
-    comment = Comment.new(comment_params)
-    comment.user_id = current_user.id
-    comment.book_id = book.id
-    if comment.save
+    @comment = Comment.new(comment_params)
+    @comment.user_id = current_user.id
+    @comment.book_id = book.id
+    if @comment.save
+      flash[:notice] = "You have posted comment successfully."
       redirect_to book_path(book)
+    else
+      @show_book = Book.find(params[:book_id])
+      @book = Book.new
+      render template: "books/show"
     end
   end
 
